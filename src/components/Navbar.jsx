f935da0ba652e38bfa2218c6da5eb014bbaba7ff
Mazid70 <mazidurmazid431@gmail.com>
2024-08-17 19:15:10 +0600

    signout added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,35 @@
+import { useContext } from 'react';
 import { IoSearch } from 'react-icons/io5';
 import { MdOutlineMenu } from 'react-icons/md';
+import { AuthContext } from '../Provider/AuthProvider';
+import { Link } from 'react-router-dom';
 
-const Navbar = ({ handleSearch, setSearch, hidden, setHidden ,isOpen,setOpen}) => {
+const Navbar = ({
+  handleSearch,
+  setSearch,
+  hidden,
+  setHidden,
+  isOpen,
+  setOpen,
+}) => {
   const handleReset = () => {
     setSearch('');
     setHidden(!hidden);
   };
-  console.log(hidden);
+  const { user,logOut } = useContext(AuthContext);
+  const handleLogOut=()=>{
+    logOut()
+    .the()
+  }
   return (
     <nav className="bg-white h-16 w-full fixed rounded-xl z-20 flex items-center shadow-md">
-      <div className="container mx-auto flex items-center ">
-      <MdOutlineMenu onClick={()=>{setOpen(!isOpen)}} className='lg:hidden text-2xl ml-5' />
+      <div className="container mx-auto flex items-center justify-between">
+        <MdOutlineMenu
+          onClick={() => {
+            setOpen(!isOpen);
+          }}
+          className="lg:hidden text-2xl ml-5"
+        />
         <h1 class="text-3xl hidden lg:block font-extrabold bg-gradient-to-r from-red-600  to-green-400 text-transparent bg-clip-text">
           Shadhin Mart
         </h1>
@@ -39,6 +58,32 @@ const Navbar = ({ handleSearch, setSearch, hidden, setHidden ,isOpen,setOpen}) =
             reset
           </button>
         </form>
+        {user ? (
+          <div className="dropdown dropdown-end">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full border-2 border-black">
+              <img
+                alt="profile"
+                src={user?.photoURL} />
+            </div>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li><a>Settings</a></li>
+            <li onClick={handleLogOut}><a>Logout</a></li>
+          </ul>
+        </div>
+          
+        ) : (
+          <Link to="/signin">Sign In</Link>
+        )}
       </div>
     </nav>
   );
